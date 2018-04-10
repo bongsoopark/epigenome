463efbf4c89441983be5dfee7772881dcdae4896
Bongsoo Park <pbsfree@gmail.com>
2018-04-09 21:36:28 -0400

    Update conf_db.php

--- lib/conf_db.php
+++ lib/conf_db.php
@@ -10,41 +10,43 @@
 # Version 1.0.0 
 #---------------------------------------------------------------------------------------------------------
 
+#PASSWORD should be stored more securely. BSPark
+
 $DBConf["PEGR_DB_TYPE"] = "MySQL";
 $DBConf["PEGR_HOST"] = "localhost";
 $DBConf["PEGR_DB"] = "pegr";
 $DBConf["PEGR_USER"] = "bongsoo";
-$DBConf["PEGR_PASS"] = "450NFrear";
+$DBConf["PEGR_PASS"] = "XXXXXXX";
 
 $DBConf["PARKLAB_DB_TYPE"] = "MySQL";
 $DBConf["PARKLAB_HOST"] = "localhost";
 $DBConf["PARKLAB_DB"] = "parklab";
 $DBConf["PARKLAB_USER"] = "bongsoo";
-$DBConf["PARKLAB_PASS"] = "450NFrear";
+$DBConf["PARKLAB_PASS"] = "XXXXXXX";
 
 $DBConf["PUGHLAB_DB_TYPE"] = "MySQL";
 $DBConf["PUGHLAB_HOST"] = "localhost";
 $DBConf["PUGHLAB_DB"] = "pughlab";
 $DBConf["PUGHLAB_USER"] = "bongsoo";
-$DBConf["PUGHLAB_PASS"] = "450NFrear";
+$DBConf["PUGHLAB_PASS"] = "XXXXXXX";
 
 $DBConf["GENOME_DB_TYPE"] = "MySQL";
 $DBConf["GENOME_HOST"] = "localhost";
 $DBConf["GENOME_DB"] = "genome";
 $DBConf["GENOME_USER"] = "bongsoo";
-$DBConf["GENOME_PASS"] = "450NFrear";
+$DBConf["GENOME_PASS"] = "XXXXXXX";
 
 $DBConf["ANALYSIS_DB_TYPE"] = "MySQL";
 $DBConf["ANALYSIS_HOST"] = "localhost";
 $DBConf["ANALYSIS_DB"] = "analysis";
 $DBConf["ANALYSIS_USER"] = "bongsoo";
-$DBConf["ANALYSIS_PASS"] = "450NFrear";
+$DBConf["ANALYSIS_PASS"] = "XXXXXXX";
 
 $DBConf["DATASET_DB_TYPE"] = "MySQL";
 $DBConf["DATASET_HOST"] = "localhost";
 $DBConf["DATASET_DB"] = "dataset";
 $DBConf["DATASET_USER"] = "bongsoo";
-$DBConf["DATASET_PASS"] = "450NFrear";
+$DBConf["DATASET_PASS"] = "XXXXXXX";
 
 function connectNewDB ($name) {
 	global $DBConf;
